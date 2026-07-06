c9b5f278ba6411ae95d333814412323c29e88dcb
andrew6005 <andrew06052015@gmail.com>
2026-07-06 17:03:26 +0800

    ex4

---
+++ demoex03/ex4/src/main/java/com/ex4/ex4/service/DocumentsService.java
@@ -0,0 +1,11 @@
+package com.ex4.ex4.service;
+
+import java.util.List;
+import org.springframework.ai.document.Document;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DocumentsService {
+  public List<Document> loaDocuments
+  
+}
